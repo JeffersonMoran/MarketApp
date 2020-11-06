95a63d19e5899f2309b64682bfcd79f867db8b25
Tiago Matos <tiagovmatos@gmail.com>
2020-11-06 20:06:22 -0300

    fix: hooks

--- src/pages/Produto/index.js
+++ src/pages/Produto/index.js
@@ -26,38 +26,36 @@ const ProdutoBox = (props) => {
   );
 }
 
-class Produto extends React.Component {
-  render() {
-    const { produto } = this.props.route.params;
-    const dispatch = useDispatch();
+const Produto = (props) => {
+  const { produto } = props.route.params;
+  const dispatch = useDispatch();
 
-    const makeLogin = async () => {
-      if (email === "" || !email_regex(email) || password === "") return alert('Ops algum campo esta vazio!');
-      await dispatch(createProductCarrinho({ email, password }))
-  }
-    
-    return (
-      <SafeAreaView style={{ backgroundColor: '#FFF', flex: 1 }}>
-        <ScrollView style={{ flex: 1, backgroundColor: '#FFF', paddingHorizontal: 16, paddingVertical: 16 }}>
-          <View>
-            <View style={{ backgroundColor: 'white' }}><Image source={{ uri: produto.imagem }} style={{ width: '100%', height: 250, resizeMode: 'contain' }} /></View>
-            <View><Text style={{ fontSize: 20, color: '#FE595E', fontWeight: 'bold', marginTop: 10 }}>{produto.nome}</Text></View>
-            <View><Text style={{ fontSize: 16, marginTop: 5, color: '#8e8e8e' }}>{produto.descricao}</Text></View>
-            <View style={{ flexDirection: 'row', marginTop: 20 }}>
-              <View style={{ flexDirection: 'row', justifyContent: 'flex-start', flex: 1 }}>
-                <TouchableOpacity style={{ backgroundColor: "#FE595E", width: 40, height: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }}><Text style={{ color: '#FFF', fontSize: 20, lineHeight: 20 }}>-</Text></TouchableOpacity>
-                <View><TextInput style={{ height: 40, width: 50, borderColor: '#ccc', borderWidth: 1, marginHorizontal: 5, textAlign: 'center' }} placeholder={'1'} /></View>
-                <TouchableOpacity style={{ backgroundColor: "#FE595E", width: 40, height: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }}><Text style={{ color: '#FFF', fontSize: 20, lineHeight: 20 }}>+</Text></TouchableOpacity>
-              </View>
-              <View style={{ flexDirection: 'row', justifyContent: 'flex-end', flex: 1 }}>
-                <TouchableOpacity style={{ backgroundColor: "green", width: 160, height: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }} onPress={() => makeLogin()}><Text style={{ color: '#FFF', fontSize: 16, lineHeight: 20 }}>ADICIONAR</Text></TouchableOpacity>
-              </View>
+  // const makeLogin = async () => {
+  //   if (email === "" || !email_regex(email) || password === "") return alert('Ops algum campo esta vazio!');
+  //   await dispatch(createProductCarrinho({ email, password }))
+  // }
+  
+  return (
+    <SafeAreaView style={{ backgroundColor: '#FFF', flex: 1 }}>
+      <ScrollView style={{ flex: 1, backgroundColor: '#FFF', paddingHorizontal: 16, paddingVertical: 16 }}>
+        <View>
+          <View style={{ backgroundColor: 'white' }}><Image source={{ uri: produto.imagem }} style={{ width: '100%', height: 250, resizeMode: 'contain' }} /></View>
+          <View><Text style={{ fontSize: 20, color: '#FE595E', fontWeight: 'bold', marginTop: 10 }}>{produto.nome}</Text></View>
+          <View><Text style={{ fontSize: 16, marginTop: 5, color: '#8e8e8e' }}>{produto.descricao}</Text></View>
+          <View style={{ flexDirection: 'row', marginTop: 20 }}>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start', flex: 1 }}>
+              <TouchableOpacity style={{ backgroundColor: "#FE595E", width: 40, height: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }}><Text style={{ color: '#FFF', fontSize: 20, lineHeight: 20 }}>-</Text></TouchableOpacity>
+              <View><TextInput style={{ height: 40, width: 50, borderColor: '#ccc', borderWidth: 1, marginHorizontal: 5, textAlign: 'center' }} placeholder={'1'} /></View>
+              <TouchableOpacity style={{ backgroundColor: "#FE595E", width: 40, height: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }}><Text style={{ color: '#FFF', fontSize: 20, lineHeight: 20 }}>+</Text></TouchableOpacity>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-end', flex: 1 }}>
+              <TouchableOpacity style={{ backgroundColor: "green", width: 160, height: 40, display: 'flex', justifyContent: 'center', alignItems: 'center' }} onPress={() => makeLogin()}><Text style={{ color: '#FFF', fontSize: 16, lineHeight: 20 }}>ADICIONAR</Text></TouchableOpacity>
             </View>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    );
-  }
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
